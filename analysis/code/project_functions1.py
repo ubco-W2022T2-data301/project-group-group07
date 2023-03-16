e74e03de50bc687c40324a8931ce69cdb49db5f4
vol2468 <vol2468@student.ubc.ca>
2023-03-15 20:39:10 -0700

    added project functoin1

---
+++ analysis/code/project_functions1.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def loadProcessRegion(path, regionName):
+  """
+  Load the data from the given path and process the data based on the given region. After processing, it returns the dataframe of the count by each company size.
+  Args:
+    path (string): the path of the file to load the file.
+    regionName (string): the name of the region to be proecessed.
+
+  Returns:
+    pandas.core.frame.DataFrame: the processed dataframe that includes the count by company size based on the region.
+  """
+  dfRegion = (
+    pd.read_csv(path)
+    .drop(columns=["work_year", "employment_type", "job_title", "salary", "salary_currency", "remote_ratio"])
+    .loc[lambda df: df["company_region"] == regionName]
+    .loc[:, ["company_size", "company_region"]]
+    .groupby("company_size")
+    .count()
+    .reset_index()
+    .rename(columns={"company_region":"count"})
+  )
+  return dfRegion
